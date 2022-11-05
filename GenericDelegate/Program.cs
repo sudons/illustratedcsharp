bb1630f6533faf5ad331e6e16707320e7d048ea9
sudons <sudons@msn.cn>
2022-11-05 23:37:41 +0800

    Func Delegate Demo

--- GenericDelegate/Program.cs
+++ GenericDelegate/Program.cs
@@ -4,21 +4,25 @@
     {
         static void Main(string[] args)
         {
-            var myDelegate = new MyDelegate<string>(Simple.PrintString);
-            myDelegate += Simple.PrintUpperString;
+            var myDelegate = new MyDelegate<string>(Simple.PrintUpperString);
             myDelegate.Invoke("Hi There");
+
+            var myDel = new Func<int, int, string>(Simple.PrintString);
+            Console.WriteLine($"Total:{myDel(13,15)}");
         }
     }
-    delegate void MyDelegate<T>(T value);
+    public delegate void MyDelegate<T>(T value);
+    public delegate TR Func<T1, T2, TR>(T1 p1, T2 p2);
     class Simple
     {
-        static public void PrintString(string s)
-        {
-            Console.WriteLine(s);
-        }
         static public void PrintUpperString(string s)
         {
             Console.WriteLine(s.ToUpper());
+        }
+        static public string PrintString(int p1,int p2)
+        {
+            int total = p1 + p2;
+            return total.ToString();
         }
     }
 }
