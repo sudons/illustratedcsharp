98a29893efdb1f4b2e8437c6fa977ebd8fbb70a5
sudons <sudons@msn.cn>
2022-11-08 22:23:47 +0800

    Modify GenericInterface code

--- GenericInterface/Program.cs
+++ GenericInterface/Program.cs
@@ -8,6 +8,10 @@
             var trivString = new Simple<string>();
             Console.WriteLine($"{trivInt.ReturnIt(5)}");
             Console.WriteLine($"{trivString.ReturnIt("Hi There.")}");
+            Console.WriteLine("----------------------------------");
+            Simple trivial = new Simple();
+            Console.WriteLine($"{trivial.ReturnIt(5)}");
+            Console.WriteLine($"{trivial.ReturnIt("Hi There.")}");
         }
     }
     interface IMyInterface<T>
@@ -17,6 +21,17 @@
     class Simple<S> : IMyInterface<S>
     {
         public S ReturnIt(S inValue)
+        {
+            return inValue;
+        }
+    }
+    class Simple : IMyInterface<int>, IMyInterface<string>
+    {
+        public int ReturnIt(int inValue)
+        {
+            return inValue;
+        }
+        public string ReturnIt(string inValue)
         {
             return inValue;
         }
