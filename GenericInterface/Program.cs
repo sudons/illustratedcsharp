508f02e23d4edf57532934e5918e5160c98488b0
sudons <sudons@msn.cn>
2022-11-08 20:44:59 +0800

    Generic Interface Demo

---
+++ GenericInterface/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var trivInt = new Simple<int>();
+            var trivString = new Simple<string>();
+            Console.WriteLine($"{trivInt.ReturnIt(5)}");
+            Console.WriteLine($"{trivString.ReturnIt("Hi There.")}");
+        }
+    }
+    interface IMyInterface<T>
+    {
+        T ReturnIt(T inValue);
+    }
+    class Simple<S> : IMyInterface<S>
+    {
+        public S ReturnIt(S inValue)
+        {
+            return inValue;
+        }
+    }
+}
