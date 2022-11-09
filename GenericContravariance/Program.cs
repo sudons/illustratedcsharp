97a7caaaa46f4b84afe260b023db9bc52945d440
sudons <sudons@msn.cn>
2022-11-09 22:02:08 +0800

    GenericContravariance

---
+++ GenericContravariance/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace GenericContravariance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<Animal> act = ActOnAnimal;
+            Action<Dog> dog = act;
+            dog(new Dog());
+        }
+        static void ActOnAnimal(Animal a)
+        {
+            Console.WriteLine(a.Legs);
+        }
+    }
+
+    internal class Animal
+    {
+        public int Legs = 4;
+    }
+    internal class Dog : Animal
+    {
+
+    }
+    delegate void Action<in T>(T a);
+}
