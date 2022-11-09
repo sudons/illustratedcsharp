b4ddcaef527a4e5e7907cf112e563999fc294f6f
sudons <sudons@msn.cn>
2022-11-09 20:55:17 +0800

    common class assignment compatibility

---
+++ GenericConvariance/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GenericConvariance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a1 = new Animal();
+            Animal a2 = new Dog();
+            Console.WriteLine($"Number of dog legs:{a2.NumberOfLegs}");
+        }
+    }
+    class Animal
+    {
+        public int NumberOfLegs = 4;
+    }
+    class Dog : Animal
+    {
+
+    }
+    /// <summary>
+    /// 关于协变和逆变的小知识
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    //泛型协变类型参数不能用于方法的输入参数类型
+    public interface IConvariance<out T>
+    {
+        public T Hit(string it);
+    }
+    //泛型逆变类型参数不能用于访求的返回值类型
+    public interface IContravariance<in T>
+    {
+        public string Hit(T it);
+    }
+}
