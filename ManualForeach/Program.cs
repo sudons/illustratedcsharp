4eeecde8341f41adab5d086610d4944704ca2adb
sudons <sudons@msn.cn>
2022-11-11 21:46:31 +0800

    ManualForeach

---
+++ ManualForeach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace ManualForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = new int[]
+            {
+                10,11,12,13
+            };
+            IEnumerator ie = intArr.GetEnumerator();
+            while (ie.MoveNext())
+            {
+                Console.WriteLine((int)ie.Current);
+            }
+        }
+    }
+}
