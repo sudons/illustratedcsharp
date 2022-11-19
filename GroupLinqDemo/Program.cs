c32d21d7cce691a41b332df56b7804bf147923dc
sudons <sudons@msn.cn>
2022-11-19 23:12:30 +0800

    在Linq中group子句返回的不是原始数据源中项的枚举，而是返回可以枚举已经形成的项的分组的可枚举类型。用人话说，就是group子句返回对象集合的集合而不是对象的集合！

---
+++ GroupLinqDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace GroupLinqDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new[]
+            {
+                new {LName="Jones",FName="Mary",Age=19,Major="History"},
+                new {LName="Smith",FName="Bob",Age=20,Major="CompSci"},
+                new {LName="Fleming",FName="Carol",Age=21,Major="History"},
+            };
+            var query = from student in students
+                        group student by student.Major;
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Key);
+                foreach (var s in item)
+                {
+                    Console.WriteLine($"--------{s.LName}-{s.FName}-{s.Age}");
+                }
+            }
+        }
+    }
+}
