47d51e0a4809b1a76bf03f996c60753aac1d69e5
sudons <sudons@msn.cn>
2022-11-19 23:25:50 +0800

    IntoLinqDemo

---
+++ IntoLinqDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IntoLinqDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var groupA = new[] { 3, 4, 5, 6 };
+            var groupB = new[] { 4,5,6,7 };
+            var someInts = from a in groupA
+                           join b in groupB
+                           on a equals b
+                           into groupAandB
+                           from c in groupAandB
+                           select c;
+            foreach (var item in someInts)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
