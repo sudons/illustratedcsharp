dc8135a29935db611b9f649b7a6368663f12d186
sudons <sudons@msn.cn>
2022-11-28 23:51:03 +0800

    学习接口的使用

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace InterfaceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animalArray = new Animal[3];
+            animalArray[0] = new Cat();
+            animalArray[1] = new Bird();
+            animalArray[2] = new Dog();
+            foreach (Animal animal in animalArray)
+            {
+                ILiveBirth liveBirth = animal as ILiveBirth;
+                if (liveBirth!=null)
+                {
+                    Console.WriteLine($"Baby is called:{liveBirth.BabyCalled()}");
+                }
+            }
+        }
+    }
+    interface ILiveBirth
+    {
+        string BabyCalled();
+    }
+    class Animal { }
+    class Cat:Animal,ILiveBirth
+    {
+        public string BabyCalled()
+        {
+            return "Kitten";
+        }
+    }
+    class Dog : Animal, ILiveBirth
+    {
+        public string BabyCalled()
+        {
+            return "puppy";
+        }
+    }
+    class Bird : Animal
+    {
+
+    }
+}
